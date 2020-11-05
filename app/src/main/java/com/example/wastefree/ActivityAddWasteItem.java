805e2daba223b135a8d9622c98765604ac155838
Tahiat Goni <tahiatg@gmail.com>
2020-11-04 23:14:23 -0700

    merging

--- app/src/main/java/com/example/wastefree/ActivityAddWasteItem.java
+++ app/src/main/java/com/example/wastefree/ActivityAddWasteItem.java
@@ -1,5 +1,6 @@
 package com.example.wastefree;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -8,15 +9,25 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Locale;
 
-public class ActivityAddWasteItem extends AppCompatActivity implements Serializable {
-
-
-    
+public class ActivityAddWasteItem extends AppCompatActivity implements Serializable, AdapterView.OnItemSelectedListener {
+    String category;
+    String desc;
+    Integer rating;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,8 +36,40 @@ public class ActivityAddWasteItem extends AppCompatActivity implements Serializa
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        final EditText itemInput = findViewById(R.id.getCategory);
         final EditText descInput = findViewById(R.id.getDescription);
+        final TextView userLoc = findViewById(R.id.location);
+        final Spinner rating = findViewById(R.id.rating);
+        final Button galleryLaunch = findViewById(R.id.gallery);
+        final Button cameraLaunch = findViewById(R.id.camera);
+
+        ArrayList<Integer> ratings = new ArrayList<>();
+        ratings.add(1);
+        ratings.add(2);
+        ratings.add(3);
+        ratings.add(4);
+        ratings.add(5);
+
+        ArrayAdapter ratelist = new ArrayAdapter(this,android.R.layout.simple_spinner_item,ratings);
+        rating.setAdapter(ratelist);
+        rating.setOnItemSelectedListener(this);
+
+
+        descInput.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                desc = descInput.getText().toString();
+            }
+        });
+
+        cameraLaunch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//                try {
+//                    startActivityForResult(camera, REQUEST_IMAGE_CAPTURE);
+//                }
+            }
+        });
 
         /**
           Getting the important information out of the other activity
@@ -43,6 +86,18 @@ public class ActivityAddWasteItem extends AppCompatActivity implements Serializa
             }
         });
 
+
+
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        rating = (Integer) parent.getItemAtPosition(position);
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
 
     }
 }
